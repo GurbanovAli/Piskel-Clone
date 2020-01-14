db47d2f152b5c8171846325bb24f0a7dd36ecacc
GurbanovAli <aligurbanov95@mail.ru>
2020-01-15 01:27:19 +0300

    add files

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Page from './components/Page';
-import './scss/index.scss';
-
-ReactDOM.render(<Page />, document.getElementById('root'));
